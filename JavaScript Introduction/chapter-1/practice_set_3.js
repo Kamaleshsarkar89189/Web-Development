91760469c279acb25a91b1ebd8595f8f1518e3a8
Kamaleshsarkar89189 <skamalesh865@gmail.com>
2024-03-13 22:24:56 +0530

    modify

--- JavaScript Introduction/chapter-1/practice_set_3.js
+++ JavaScript Introduction/chapter-1/practice_set_3.js
@@ -1,7 +1,7 @@
 //Create a const object in javascript . Can you change it to hold a number later?
  const a = {
-    name : Kamalesh,
-    section : E,
+    name : "Kamalesh",
+    section : "E",
     isKisan : true
  }
 //  a = 45 //can't change it to hold it later.
