c54612d3270d52a74af0227da3cddb29d93cf0c1
Kamaleshsarkar89189 <skamalesh865@gmail.com>
2024-09-01 00:11:36 +0530

    added my-app

---
+++ React Hooks/my-app/src/components/UseState.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const UseState = () => {
+    return (
+        <div>
+            <div>
+                <h1>
+                    <center>
+                        Hello, World!
+                    </center>
+                </h1>
+            </div>
+        </div>
+    )
+}
